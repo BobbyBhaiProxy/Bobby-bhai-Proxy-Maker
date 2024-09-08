1e3a75670a5a0a39e0f7dd8c3421cedd688b780b
BobbyBhaiProxy <kiskomailkardiye@gmail.com>
2024-09-08 21:12:25 +0530

    Update squid-uninstall.sh

--- squid-uninstall.sh
+++ squid-uninstall.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 
 ############################################################
-# Squid Proxy Installer
-# Author: Yujin Boby
-# Email: admin@serverOk.in
-# Github: https://github.com/serverok/squid-proxy-installer/
-# Web: https://serverok.in/squid
-# If you need professional assistance, reach out to
-# https://serverok.in/contact
+# Squid Proxy Uninstaller - Bobby Bhai Proxy Maker
 ############################################################
 
+# Check if the script is running as root
 if [ `whoami` != root ]; then
-	echo "ERROR: You need to run the script as user root or add sudo before command."
-	exit 1
+    echo "ERROR: You need to run the script as root or add sudo before the command."
+    exit 1
 fi
 
+# Ensure the OS detection script exists
 if [ ! -f /usr/bin/sok-find-os ]; then
     echo "/usr/bin/sok-find-os not found"
     exit 1
 fi
 
-rm -f /root/squid3-install.sh > /dev/null 2>&1
-
+# Use sok-find-os to detect the OS
 SOK_OS=$(/usr/bin/sok-find-os)
 
+# Check if the OS is supported
 if [ $SOK_OS == "ERROR" ]; then
     cat /etc/*release
     echo
@@ -31,77 +27,38 @@ if [ $SOK_OS == "ERROR" ]; then
     echo -e "\e[1;31mOS NOT SUPPORTED"
     echo -e "\e[1;31m====================================================="
     echo -e "\e[0m"
-    echo -e "Contact me to add support for your OS.\n"
-    echo -e "https://serverok.in/contact\n"
-    exit 1;
+    echo -e "Contact us to add support for your OS.\n"
+    echo -e "https://github.com/BobbyBhaiProxy\n"
+    exit 1
 fi
 
-if [ $SOK_OS == "ubuntu2404" ]; then
+# Uninstall Squid Proxy based on OS
+if [ $SOK_OS == "ubuntu2404" ] || [ $SOK_OS == "ubuntu2204" ] || [ $SOK_OS == "ubuntu2004" ]; then
     /usr/bin/apt -y remove --purge squid squid-common squid-langpack
     rm -rf /etc/squid/
-elif [ $SOK_OS == "ubuntu2204" ]; then
-    /usr/bin/apt -y remove --purge squid squid-common squid-langpack
-    rm -rf /etc/squid/
-elif [ $SOK_OS == "ubuntu2004" ]; then
-    /usr/bin/apt -y remove --purge squid squid-common squid-langpack
-    rm -rf /etc/squid/
-elif [ $SOK_OS == "ubuntu1804" ]; then
+elif [ $SOK_OS == "ubuntu1804" ] || [ $SOK_OS == "ubuntu1604" ]; then
     /usr/bin/apt -y remove --purge squid3
-    /bin/rm -rf /etc/squid/
-elif [ $SOK_OS == "ubuntu1604" ]; then
-    /usr/bin/apt -y remove --purge squid3
-    /bin/rm -rf /etc/squid3/
-    /bin/rm -rf /etc/squid/
+    rm -rf /etc/squid/
 elif [ $SOK_OS == "ubuntu1404" ]; then
     /usr/bin/apt remove --purge squid3 -y
-    /bin/rm -rf /etc/squid3/
-    /bin/rm -rf /etc/squid/
-elif [ $SOK_OS == "debian8" ]; then
-    echo "Uninstalling Squid Proxy on Debian 8"
-    /usr/bin/apt -y remove --purge squid
-    /bin/rm -rf /etc/squid3/
-    /bin/rm -rf /etc/squid/
-elif [ $SOK_OS == "debian9" ]; then
-    echo "Uninstalling Squid Proxy on Debian 9"
-    /usr/bin/apt -y remove --purge squid
-    /bin/rm -rf /etc/squid/
-    /bin/rm -rf /var/spool/squid
-elif [ $SOK_OS == "debian10" ]; then
-    echo "Uninstalling Squid Proxy on Debian 10"
+    rm -rf /etc/squid3/ /etc/squid/
+elif [[ $SOK_OS == debian* ]]; then
+    echo "Uninstalling Squid Proxy on $SOK_OS"
     /usr/bin/apt -y remove --purge squid squid-common squid-langpack
-    /bin/rm -rf /etc/squid/
-    /bin/rm -rf /var/spool/squid
-elif [ $SOK_OS == "debian11" ]; then
-    echo "Uninstalling Squid Proxy on Debian 11"
-    /usr/bin/apt -y remove --purge squid squid-common squid-langpack
-    /bin/rm -rf /etc/squid/
-    /bin/rm -rf /var/spool/squid
-elif [ $SOK_OS == "debian12" ]; then
-    echo "Uninstalling Squid Proxy on Debian 12"
-    /usr/bin/apt -y remove --purge squid squid-common squid-langpack
-    /bin/rm -rf /etc/squid/
-    /bin/rm -rf /var/spool/squid
-elif [ $SOK_OS == "centos7" ]; then
-    yum remove squid -y
-    /bin/rm -rf /etc/squid/
-elif [ "$SOK_OS" == "centos8" ] || [ "$SOK_OS" == "almalinux8" ] || [ "$SOK_OS" == "almalinux9" ]; then
-    yum remove squid -y
-    /bin/rm -rf /etc/squid/
-elif [ "$SOK_OS" == "centos8s" ]; then
-    dnf remove squid -y
-    /bin/rm -rf /etc/squid/
-elif [ "$SOK_OS" == "centos9" ]; then
-    dnf remove squid -y
-    /bin/rm -rf /etc/squid/
+    rm -rf /etc/squid/ /var/spool/squid
+elif [[ $SOK_OS == centos* ]] || [[ $SOK_OS == almalinux* ]]; then
+    yum remove squid -y || dnf remove squid -y
+    rm -rf /etc/squid/
+else
+    echo "OS not supported for uninstallation."
+    exit 1
 fi
 
-rm -f /usr/bin/squid-add-user > /dev/null 2>&1
-rm -f /usr/bin/sok-find-os > /dev/null 2>&1
-rm -f /usr/bin/squid-uninstall > /dev/null 2>&1
+# Remove additional custom scripts
+rm -f /usr/bin/squid-add-user /usr/bin/sok-find-os /usr/bin/squid-uninstall > /dev/null 2>&1
 
-echo 
-echo 
+echo
 echo "Squid Proxy uninstalled."
-echo "Thank you for using Bobby Bhai Proxy Maker"
-echo "If you want to reinstall Squid Proxy Server, check https://github.com/BobbyBhaiProxy"
-echo 
+echo "Thank you for using Bobby Bhai Proxy Maker!"
+echo "To reinstall Squid Proxy, visit https://github.com/BobbyBhaiProxy."
+echo
