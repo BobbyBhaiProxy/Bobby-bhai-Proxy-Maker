9d16b96601e40dae5df9351e072e59859b2e8e7b
BobbyBhaiProxy <kiskomailkardiye@gmail.com>
2024-09-10 17:33:05 +0530

    Update sok-find-os.sh

--- sok-find-os.sh
+++ sok-find-os.sh
@@ -19,18 +19,18 @@ elif grep -q "Ubuntu 14.04" /etc/os-release; then
     echo "ubuntu1404"
 
 # Check for Debian versions
-elif grep -q "jessie" /etc/os-release; then
-    echo "debian8"
-elif grep -q "stretch" /etc/os-release; then
-    echo "debian9"
-elif grep -q "buster" /etc/os-release; then
-    echo "debian10"
-elif grep -q "bullseye" /etc/os-release; then
-    echo "debian11"
 elif grep -q "bookworm" /etc/os-release; then
     echo "debian12"
+elif grep -q "bullseye" /etc/os-release; then
+    echo "debian11"
+elif grep -q "buster" /etc/os-release; then
+    echo "debian10"
+elif grep -q "stretch" /etc/os-release; then
+    echo "debian9"
+elif grep -q "jessie" /etc/os-release; then
+    echo "debian8"
 
-# Check for CentOS and AlmaLinux versions
+# Check for CentOS, AlmaLinux, Rocky Linux versions
 elif grep -q "CentOS Linux 7" /etc/os-release; then
     echo "centos7"
 elif grep -q "CentOS Linux 8" /etc/os-release; then
@@ -43,6 +43,34 @@ elif grep -q "CentOS Stream 8" /etc/os-release; then
     echo "centos8s"
 elif grep -q "CentOS Stream 9" /etc/os-release; then
     echo "centos9"
+elif grep -q "Rocky Linux 8" /etc/os-release; then
+    echo "rockylinux8"
+elif grep -q "Rocky Linux 9" /etc/os-release; then
+    echo "rockylinux9"
+
+# Check for Fedora versions
+elif grep -q "Fedora release" /etc/os-release; then
+    echo "fedora"
+
+# Check for Alpine Linux
+elif grep -q "Alpine Linux" /etc/os-release; then
+    echo "alpine"
+
+# Check for Arch Linux
+elif grep -q "Arch Linux" /etc/os-release; then
+    echo "archlinux"
+
+# Check for openSUSE
+elif grep -q "openSUSE Leap 15" /etc/os-release; then
+    echo "opensuse15"
+
+# Check for FreeBSD
+elif grep -q "FreeBSD" /etc/os-release; then
+    echo "freebsd"
+
+# Check for OpenBSD
+elif grep -q "OpenBSD" /etc/os-release; then
+    echo "openbsd"
 
 # If no match, return error
 else
